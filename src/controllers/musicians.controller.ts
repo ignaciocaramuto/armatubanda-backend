93b341046ab75f43dd085db0377d13b59f43535b
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-03-19 17:50:41 -0300

    getAll + create

--- src/controllers/musicians.controller.ts
+++ src/controllers/musicians.controller.ts
@@ -1,10 +1,17 @@
 import { Request, Response } from "express";
+import { orm } from "../shared/db/orm.js";
 import { Musician } from "../models/musician.entity.js";
+
+const em = orm.em;
 
 export class MusicianController {
   static async getAll(req: Request, res: Response) {
-    const musicians = "";
-    return res.json(musicians);
+    try {
+      const musicians = await em.find(Musician, {});
+      res.status(200).json(musicians);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
   }
 
   static async getById(req: Request, res: Response) {
@@ -18,7 +25,15 @@ export class MusicianController {
     res.json(musician);
   }
 
-  static async create(req: Request, res: Response) {}
+  static async create(req: Request, res: Response) {
+    try {
+      const musician = em.create(Musician, req.body);
+      await em.flush();
+      res.status(201).json(musician);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  }
 
   static async update(req: Request, res: Response) {}
 }

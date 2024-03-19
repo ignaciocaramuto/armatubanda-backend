93271963b0d954c6dd4098bf0f92ca5026994269
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-03-19 17:50:12 -0300

    auth

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express, { json } from "express";
 import "reflect-metadata";
 import { orm, syncSchema } from "./shared/db/orm.js";
 import { RequestContext } from "@mikro-orm/core";
-import { musicianRouter } from "./routes/musicians.js";
+import { musicianRouter } from "./routes/musicians.route.js";
+import { authRouter } from "./routes/auth.route.js";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use((req, res, next) => {
   RequestContext.create(orm.em, next); //em = Entity Manager: abstraccion que permite manejar todas las entidades que definimos y nos permite manejarlas uniformemente (unit of work)
 });
 
+app.use("/auth", authRouter);
 app.use("/musician", musicianRouter);
 
 await syncSchema(); // Never in prod
---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,71 @@
+import { Request, Response } from "express";
+import { orm } from "../shared/db/orm.js";
+import { Musician } from "../models/musician.entity.js";
+import { Role } from "../enums/role.enum.js";
+import { createHash } from "crypto";
+
+const em = orm.em;
+
+export class AuthController {
+  static async register(req: Request, res: Response) {
+    const { email, password } = req.body;
+    AuthController.requiredFieldsValidation(email, password, res);
+
+    const duplicate = await em.findOne(Musician, { email });
+    if (duplicate) {
+      return res
+        .status(409)
+        .json({ message: `El email ${email} ya fue utilizado.` });
+    }
+
+    const hashedPassword = createHash("md5").update(password).digest("hex");
+
+    const musician: any = {
+      email,
+      password: hashedPassword,
+      role: Role.USER,
+      isProfileSet: false,
+      isEmailConfirmed: false,
+    };
+
+    try {
+      const registeredMusician = em.create(Musician, musician);
+      await em.flush();
+      res.status(201).json(registeredMusician);
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  static async login(req: Request, res: Response) {
+    const { email, password } = req.body;
+    AuthController.requiredFieldsValidation(email, password, res);
+
+    try {
+      const musician = await em.findOneOrFail(Musician, { email });
+      const hashedPassword = createHash("md5").update(password).digest("hex");
+
+      if (hashedPassword !== musician.password) {
+        return res
+          .status(401)
+          .json({ message: "Usuario y/o contraseña incorrectos" });
+      }
+
+      res.status(200).json({ token: "TEST" });
+    } catch (error: any) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  static requiredFieldsValidation(
+    email: string,
+    password: string,
+    res: Response
+  ) {
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Usuario y contraseña son requeridos." });
+    }
+  }
+}

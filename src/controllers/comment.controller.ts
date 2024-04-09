12fef5684989e6deb31e063f4fc2b2dc7189fb55
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-04-09 16:22:43 -0300

    implemented comment controller logic

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -5,20 +5,37 @@ import { Comment } from "../models/comment.entity.js";
 const em = orm.em;
 
 export class CommentController {
-  static async getAll(req: Request, res: Response) {
+  static async getAllFromMusician(req: Request, res: Response) {
     const id = Number.parseInt(req.params.id);
     const comments = await em.find(Comment, { musician: id });
     res.status(200).json(comments);
   }
 
-  static async create(req: Request, res: Response) {
-    const { comment } = req.body;
+  static async getAllFromBand(req: Request, res: Response) {
     const id = Number.parseInt(req.params.id);
-    const authorId = req.user.id;
+    const comments = await em.find(Comment, { band: id });
+    res.status(200).json(comments);
+  }
 
-    em.create(Comment, { comment, musician: id, author: authorId });
+  static async createForMusician(req: Request, res: Response) {
+    const musicianId = Number.parseInt(req.params.id);
+    const { comment } = req.body;
+    const { id } = req.user;
+
+    em.create(Comment, { comment, author: id, musician: musicianId });
     await em.flush();
 
-    res.status(201).json({ message: "Comentario agregado correctamente" });
+    res.status(201).json({ message: "Comentario realizado correctamente" });
+  }
+
+  static async createForBand(req: Request, res: Response) {
+    const bandId = Number.parseInt(req.params.id);
+    const { comment } = req.body;
+    const { id } = req.user;
+
+    em.create(Comment, { comment, author: id, band: bandId });
+    await em.flush();
+
+    res.status(201).json({ message: "Comentario realizado correctamente" });
   }
 }

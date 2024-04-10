6ba26ae02b5c210e33c321e6e162267c660960b4
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-04-09 21:47:51 -0300

    fix + added new methods

--- src/controllers/musicians.controller.ts
+++ src/controllers/musicians.controller.ts
@@ -3,6 +3,9 @@ import { orm } from "../shared/db/orm.js";
 import { Musician } from "../models/musician.entity.js";
 import { Experience } from "../enums/experience.enum.js";
 import { Career } from "../models/career.entity.js";
+import { Invitation } from "../models/invitation.entity.js";
+import { Status } from "../enums/status.enum.js";
+import { MusicianBandStatus } from "../enums/musician-band-status.enum.js";
 
 const em = orm.em;
 export class MusicianController {
@@ -46,27 +49,34 @@ export class MusicianController {
     res.status(200).json(musician);
   }
 
+  // Update entire entity
   static async createUpdateProfile(req: Request, res: Response) {
     const emFork = em.fork();
     const { id } = req.user;
-    const { career } = req.body;
     const musician = await emFork.findOneOrFail(Musician, id);
 
-    let updatedMusician = {
-      ...req.body,
+    const updated = new Musician();
+    Object.assign(updated, {
       isProfileSet: true,
-    };
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      stageName: req.body.stageName,
+      country: req.body.country,
+      state: req.body.state,
+      city: req.body.city,
+      bio: req.body.bio,
+      genres: req.body.genres,
+      instruments: req.body.instruments,
+      image: req.body.image,
+      phoneNumber: req.body.phoneNumber,
+      birthday: req.body.birthday,
+      webSite: req.body.webSite,
+      socialMedia: req.body.socialMedia,
+      imagePath: req.file?.path,
+    });
 
-    if (req.file?.path) {
-      updatedMusician = { ...updatedMusician, imagePath: req.file.path };
-    }
-
-    if (career) {
-      if (!musician.career.isInitialized()) {
-        await musician.career.init();
-      }
-
-      const careers = JSON.parse(career);
+    if (req.body.career) {
+      const careers = JSON.parse(req.body.career);
       const careersMap = careers.map(
         ({ title, description, startDate, endDate }: any) => {
           const careerObj = new Career();
@@ -81,15 +91,12 @@ export class MusicianController {
         }
       );
 
-      careersMap.forEach((career: Career) => {
-        musician.career.add(career);
+      Object.assign(updated, {
+        career: careersMap,
       });
-
-      updatedMusician = { ...updatedMusician, career: musician.career };
     }
 
-    emFork.assign(musician, updatedMusician);
-
+    await emFork.assign(musician, updated);
     await emFork.flush();
     res.status(200).json(musician);
   }
@@ -103,5 +110,65 @@ export class MusicianController {
     );
 
     res.status(200).json({ bio, career, instruments, lookingBands });
+  }
+
+  static async getBandsFromMusician(req: Request, res: Response) {
+    const id = Number.parseInt(req.params.id);
+    const { bands, ledBands } = await em.findOneOrFail(
+      Musician,
+      { id },
+      { populate: ["bands", "ledBands"] }
+    );
+
+    res.status(200).json([...bands, ...ledBands]);
+  }
+
+  static async getBandsFromLeaderToInviteMusician(req: Request, res: Response) {
+    const { id } = req.user;
+    const musicianToInviteId = Number.parseInt(req.params.id);
+    const { ledBands } = await em.findOneOrFail(
+      Musician,
+      { id },
+      { populate: ["ledBands"] }
+    );
+
+    const invitations = await em.find(Invitation, {
+      musician: musicianToInviteId,
+      status: Status.PENDING,
+    });
+
+    const pendingInvitationsMap: { [bandId: number]: boolean } = {};
+    invitations.forEach((invitation: Invitation) => {
+      pendingInvitationsMap[invitation.band.id] = true;
+    });
+
+    const bands = ledBands.map((band) => {
+      let isMember = false;
+      if (band.members.isInitialized()) {
+        isMember = band.members
+          .getItems()
+          .some(({ id }: Musician) => id === musicianToInviteId);
+      }
+
+      const hasPendingInvitation = pendingInvitationsMap[band.id] || false;
+
+      let status = "";
+      if (isMember) {
+        status = MusicianBandStatus.MEMBER;
+      } else if (hasPendingInvitation) {
+        status = MusicianBandStatus.PENDING_INVITATION;
+      } else {
+        status = MusicianBandStatus.NOT_MEMBER;
+      }
+
+      return {
+        id: band.id,
+        name: band.name,
+        imagePath: band.imagePath,
+        status: status,
+      };
+    });
+
+    res.status(200).json(bands);
   }
 }

eac02dd1a6172f64b6cb14accae7db34beaf8557
Ignacio Caramuto <ignaciocaramuto@gmail.com>
2024-04-09 23:20:09 -0300

    added status condition

--- src/controllers/advertisements.controller.ts
+++ src/controllers/advertisements.controller.ts
@@ -3,19 +3,89 @@ import { orm } from "../shared/db/orm.js";
 import { Band } from "../models/band.entity.js";
 import { Advertisement } from "../models/advertisement.entity.js";
 import { Status } from "../enums/status.enum.js";
+import { Musician } from "../models/musician.entity.js";
+import { AdvertisementStatus } from "../enums/advertisement-status.js";
 
 const em = orm.em;
 
 export class AdvertisementController {
   static async getAllFromBand(req: Request, res: Response) {
-    const id = Number.parseInt(req.params.id);
+    const musicianId = req.user.id;
+    const { name, instruments, genres } = req.query;
 
-    const advertisements = await em.find(
-      Advertisement,
-      { band: id },
-      { populate: ["band"] }
+    const filters: Record<string, any> = {};
+
+    if (name) filters.name = { $like: `%${name}%` };
+    if (instruments)
+      filters.instruments = { $in: (instruments as string).split(",") };
+    if (genres) filters.genres = { $in: (genres as string).split(",") };
+
+    const advertisements = await em.find(Advertisement, filters, {
+      populate: ["band", "applications"],
+    });
+
+    const ads = advertisements.map(
+      ({
+        id,
+        title,
+        description,
+        band,
+        applications,
+        createdAt,
+        genres,
+        instruments,
+      }: Advertisement) => {
+        let isMember = false;
+        let hasPendingApplication = false;
+        let hasPendingInvitation = false;
+
+        const isLeader = band.leader.id === musicianId;
+
+        if (band.members.isInitialized()) {
+          isMember = band.members
+            .getItems()
+            .some((musician: Musician) => musician.id === musicianId);
+        }
+
+        if (applications.isInitialized()) {
+          hasPendingApplication = applications
+            .getItems()
+            .some(
+              ({ applicant, status }) =>
+                applicant.id === musicianId && status === Status.PENDING
+            );
+        }
+
+        if (band.invitations.isInitialized()) {
+          hasPendingInvitation = band.invitations
+            .getItems()
+            .some(({ musician }) => musician.id === musicianId);
+        }
+
+        let status = AdvertisementStatus.ELIGIGLE;
+
+        if (isMember || isLeader) {
+          status = AdvertisementStatus.MEMBER;
+        } else if (hasPendingApplication) {
+          status = AdvertisementStatus.PENDING_APPLICATION;
+        } else if (hasPendingInvitation) {
+          status = AdvertisementStatus.PENDING_INVITATION;
+        }
+
+        return {
+          id,
+          title,
+          description,
+          createdAt,
+          band,
+          genres,
+          instruments,
+          status,
+        };
+      }
     );
-    res.status(200).json(advertisements);
+
+    res.status(200).json(ads);
   }
 
   static async create(req: Request, res: Response) {
@@ -35,12 +105,10 @@ export class AdvertisementController {
 
   static async delete(req: Request, res: Response) {
     const { id } = req.user;
-    const bandId = Number.parseInt(req.params.bandId);
     const adId = Number.parseInt(req.params.id);
-
-    await em.findOneOrFail(Band, { id: bandId, leader: id });
-
     const advertisement = await em.findOneOrFail(Advertisement, { id: adId });
+
+    await em.findOneOrFail(Band, { id: advertisement.band.id, leader: id });
     await em.removeAndFlush(advertisement);
     res.status(200).json({ message: "Aviso eliminado correctamente" });
   }
